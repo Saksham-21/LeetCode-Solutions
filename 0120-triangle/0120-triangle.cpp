74089164ac8a5e07dbb18ba6b61e710a49a8fc56
Saksham-21 <91721691+Saksham-21@users.noreply.github.com>
2024-01-16 00:45:20 +0530

    Time: 3 ms (89.56%), Space: 9.3 MB (36.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -8,23 +8,20 @@ public:
             for(int j=0;j<x;j++){
                 if(i==0 && j==0) {
                     dp[i][j]=triangle[i][j];
-                    cout<<"DP"<<i<<j<<"   "<<dp[i][j]<<endl;
+                    // cout<<"DP"<<i<<j<<"   "<<dp[i][j]<<endl;
                 }
                 else{
                     int up=triangle[i][j]+((i!=j)?dp[i-1][j]:1e9);
                     int dia=triangle[i][j]+((j!=0)?dp[i-1][j-1]:1e9);
-                    cout<<"UP--"<<up<<" "<<"DIA--"<<dia<<endl;
+                    // cout<<"UP--"<<up<<" "<<"DIA--"<<dia<<endl;
                 
                 
                     dp[i][j]=min(up,dia);
-                    cout<<"\n";
-                    cout<<"DP"<<i<<j<<"   "<<dp[i][j]<<endl;   
+                    // cout<<"\n";
+                    // cout<<"DP"<<i<<j<<"   "<<dp[i][j]<<endl;   
                 }
                  
             }
-        }
-        for(int i=0;i<m;i++){
-            cout<<dp[m-1][i]<<" ";
         }
         int minTotal = INT_MAX;
         for (int i = 0; i < m; i++) {
