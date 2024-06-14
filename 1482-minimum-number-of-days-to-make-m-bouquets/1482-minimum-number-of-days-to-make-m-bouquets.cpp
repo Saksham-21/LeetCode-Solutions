d95f12a8c28497b28eec9c29c1b41543436968ad
Saksham-21 <91721691+Saksham-21@users.noreply.github.com>
2024-06-14 11:53:05 +0530

    Time: 112 ms (59.10%), Space: 69 MB (28.82%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool ispossible(vector<int>& vc,int m,int k,int mid){
+        int cnt=0;
+        int no_of_bouquets=0;
+        for(int i=0;i<vc.size();i++){
+            if(vc[i]<=mid){
+                cnt++;
+            }else{
+                no_of_bouquets+=(cnt/k);
+                cnt=0;
+            }
+        }
+        no_of_bouquets+=(cnt/k);
+        return no_of_bouquets>=m;
+    }
+    int minDays(vector<int>& vec, int m, int k) {
+        if(vec.size()< static_cast<long long>(m) * k){
+            return -1;
+        }
+        int low=*min_element(vec.begin(),vec.end());
+        int high=*max_element(vec.begin(),vec.end());
+        int ans=INT_MAX;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(ispossible(vec,m,k,mid)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;   
+            }
+        }
+        return ans;
+    }
+};
