c0f50f7851145bc9c6c6bab9fff570f1945d35df
Saksham-21 <91721691+Saksham-21@users.noreply.github.com>
2024-06-18 13:46:58 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (7.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    //     vector<int> ans;
-    //     int i=0,j=0;
-    //     while(i<m && j<n){
-    //         if(nums1[i]<nums2[j]){
-    //             ans.push_back(nums1[i]);
-    //             i++;
-    //         }else{
-    //             ans.push_back(nums2[j]);
-    //             j++;
-    //         }
-    //     }
-    //     nums1=ans;
-    // }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left=m-1;
-        int right=0;
-        while(left>=0 && right<n){
-            if(nums1[left]>nums2[right]){
-                swap(nums1[left],nums2[right]);
-                left--;
-                right++;
+        vector<int> ans;
+        int i=0,j=0;
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
             }else{
-                break;
+                ans.push_back(nums2[j]);
+                j++;
             }
         }
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[i-m];
+        while(i<m){
+            ans.push_back(nums1[i]);
+            i++;
         }
-        sort(nums1.begin(),nums1.end());
+        while(j<n){
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        nums1=ans;
     }
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     int left=m-1;
+    //     int right=0;
+    //     while(left>=0 && right<n){
+    //         if(nums1[left]>nums2[right]){
+    //             swap(nums1[left],nums2[right]);
+    //             left--;
+    //             right++;
+    //         }else{
+    //             break;
+    //         }
+    //     }
+    //     for(int i=m;i<m+n;i++){
+    //         nums1[i]=nums2[i-m];
+    //     }
+    //     sort(nums1.begin(),nums1.end());
+    // }
 };
