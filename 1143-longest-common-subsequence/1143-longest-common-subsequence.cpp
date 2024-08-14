5cc493c5d047acf80dcb70687832418030c070bb
Saksham-21 <91721691+Saksham-21@users.noreply.github.com>
2024-08-14 14:42:16 +0530

    Time: 15 ms (93.94%), Space: 8 MB (91.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -17,22 +17,40 @@ public:
         // return f(text1,text2,n-1,m-1,dp);
         
         
-        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0]=0;
+        // }
+        // for(int j=0;j<=m;j++){
+        //     dp[0][j]=0;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(text1[i-1]==text2[j-1]){
+        //             dp[i][j]=1+dp[i-1][j-1];
+        //         }else{
+        //             dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+        
+        
+        
+        vector<int> pre(m+1,0),curr(m+1,0);
+        for(int i=0;i<=m;i++){
+            pre[i]=0;
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+pre[j-1];
                 }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=max(pre[j],curr[j-1]);
                 }
             }
+            pre=curr;
         }
-        return dp[n][m];
+        return pre[m];
     }
 };
